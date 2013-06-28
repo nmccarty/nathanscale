1f64e5369a23dec7a3eecad8800921bf6ce9cee2
nmccarty <nmccarty@localhost>
2013-06-28 17:50:22 -0400

    Added 3 math functions and a high precision definition of pi.

--- src/NathanScale/core.clj
+++ src/NathanScale/core.clj
@@ -1,6 +1,20 @@
 (ns NathanScale.core)
 
-(defn foo
-  "I don't do a whole lot."
+(def PI 3.141592653589793)
+
+(defn sinc 
+  "Normalized sinc function"
   [x]
-  (println x "Hello, World!"))
+  (if (= 0 x)
+    1
+    (/ (Math/sin (* PI x)) (* PI x))))
+
+(defn sinc-2d
+  "Extension of the sinc function to two dimensions"
+  [x y]
+  (* (sinc x) (sinc y)))
+
+(defn clamp
+  "Clamps n to the range defined by lower and upper"
+  [lower upper n]
+  (max lower (min n upper)))
